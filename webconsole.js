946bb245d52118e8494c7a5d3263227273cc7be9
范培伦 <fanpeilun@163.com>
2019-03-11 19:40:44 +0800

    Update webconsole.js

--- webconsole.js
+++ webconsole.js
@@ -15,4 +15,22 @@
       href: "https://yun.baidu.com/s/1xIwYILHQebEHZOcW4yvsAw",
       tooltip: "一键部署Openshift相关视频"
     }];
+    
+    window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS = [{
+      title: "Dashboard",                         // The text label
+      icon: "fa fa-dashboard",                    // The icon you want to appear
+      url: "http://example.com/dashboard",        // Where to go when this item is clicked
+      description: "Open application dashboard."  // Short description
+    }, {
+      title: "System Status",
+      icon: "fa fa-heartbeat",
+      url: "http://example.com/status",
+      description: "View system alerts and outages."
+    }, {
+      title: "Manage Account",
+      icon: "pficon pficon-user",
+      url: "http://example.com/account",
+      description: "Update email address or password."
+    }];
+    
 }());
