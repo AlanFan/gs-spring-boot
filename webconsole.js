330c0be3b2538167fde7c456a6f6725ea843f0b0
范培伦 <fanpeilun@163.com>
2019-03-11 19:07:43 +0800

    Update webconsole.js

--- webconsole.js
+++ webconsole.js
@@ -1,5 +1,6 @@
 (function() {
     window.OPENSHIFT_CONSTANTS.PROJECT_NAVIGATION[0].label="概览"
+    window.OPENSHIFT_CONSTANTS.PROJECT_NAVIGATION[0].secondaryNavSections[0].items[0].label="部署"
     window.OPENSHIFT_CONSTANTS.PROJECT_NAVIGATION[1].label="应用"
     window.OPENSHIFT_CONSTANTS.PROJECT_NAVIGATION[2].label="构建"
     window.OPENSHIFT_CONSTANTS.PROJECT_NAVIGATION[3].label="资源"
